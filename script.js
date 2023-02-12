a79e16fc29faa24afe8f65f8dbca53624d7c9774
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-02-12 19:08:30 +1100

    Add form validation

---
+++ script.js
@@ -0,0 +1,31 @@
+const passwordInput = document.getElementById("password")
+const passwordConfirmInput = document.getElementById("passwordConfirm")
+const submit = document.getElementById("submit")
+const invalidPasswordMessage = document.getElementById("invalid-password")
+
+const invalidInputOutline = "solid 1px red"
+const defaultInputOutline = passwordInput.style.outline
+
+submit.onclick = (e) => {
+    if (passwordInput.value != passwordConfirmInput.value){
+        displayInvalidPassword(true)
+        e.preventDefault();
+    }
+}
+
+passwordInput.onclick = (e) => {
+    displayInvalidPassword(false)
+}
+
+function displayInvalidPassword(isInvalid){
+    if (isInvalid){
+        passwordConfirmInput.style.outline = "solid 1px red"
+        passwordInput.style.outline = "solid 1px red"
+        invalidPasswordMessage.style.visibility = "visible"
+    }
+    else {
+        passwordConfirmInput.style.outline = defaultInputOutline
+        passwordInput.style.outline = defaultInputOutline
+        invalidPasswordMessage.style.visibility = "hidden"
+    }
+}
